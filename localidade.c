521b2bda805a3229bb839ad641039fe1fb765adc
Adilson Pedro <pedroadilson513@gmail.com>
2023-06-30 20:15:22 +0100

    funções prontas, excepto 'ligacoes otimizads'

--- localidade.c
+++ localidade.c
@@ -12,7 +12,7 @@
     Funcao para criar o grafo inicial(Localidade)
 */
 No * novo_no(int destino, int distancia){
-
+    
     No * no = (No *)malloc(sizeof(No));
     if(!no){
         printf("Erro: não foi possível criar um novo 'NO'");
@@ -26,6 +26,14 @@ No * novo_no(int destino, int distancia){
 }
 
 Localidade *criarLocalidade(int pontos, int ruas){
+    if(pontos < 0){
+        printf("Número de pontos inválido.\n");
+        return NULL;
+    }
+    if(ruas < 0){
+        printf("Número de ruas inválidas.\n");
+        return NULL;
+    }
     Localidade *nova_localidade = (Localidade *)malloc(sizeof(Localidade));
     
     nova_localidade->numero_pontos = pontos;
@@ -50,6 +58,14 @@ Localidade *criarLocalidade(int pontos, int ruas){
     Funcao para remover vértice(PONTO) do grafo (LOCALIDADE)
 */
 void removerPonto(Localidade *localidade, int ponto){
+    if(localidade == NULL){
+        printf("Localidade inexistente\n");
+        return;
+    }
+    if(!(localidade->numero_pontos>=ponto && ponto>0)){
+        printf("Ponto inválido.\n");
+        return;
+    }
     No *aux;
     /*
         Para cada lista associada a um ponto, remove-se o ponto a ser
@@ -93,6 +109,18 @@ void removerPonto(Localidade *localidade, int ponto){
     Funcao para adicionar rua(ARESTA -> (A <---> B => distancia)) do grafo (LOCALIDADE)
 */
 void adicionarRua(Localidade *localidade, int pontoA, int pontoB, int distancia){
+    if(localidade == NULL){
+        printf("Localidade inexistente\n");
+        return;
+    }
+    if(!(localidade->numero_pontos>=pontoA && pontoA>0)){
+        printf("Ponto A inválido.\n");
+        return;
+    }
+    if(!(localidade->numero_pontos>=pontoB && pontoB>0)){
+        printf("Ponto B inválido.\n");
+        return;
+    }
     No * aux = NULL;
     No *novoNo = novo_no(pontoB, distancia);
 
@@ -124,13 +152,71 @@ void adicionarRua(Localidade *localidade, int pontoA, int pontoB, int distancia)
 /*
     Funcao para remover rua(ARESTA) do grafo (LOCALIDADE)
 */
-Localidade *removerRua(Localidade *localidade, int pontoA, int pontoB){
-    
+void removerRua(Localidade *localidade, int pontoA, int pontoB){
+    if(localidade == NULL){
+        printf("Localidade inexistente\n");
+        return;
+    }
+    if(!(localidade->numero_pontos>=pontoA && pontoA>0)){
+        printf("Ponto A inválido.\n");
+        return;
+    }
+    if(!(localidade->numero_pontos>=pontoB && pontoB>0)){
+        printf("Ponto B inválido.\n");
+        return;
+    }
+    No *aux = localidade->ruas[pontoA].principal;
+    while (aux != NULL && aux->destino == pontoB) {
+            No *temp = aux;
+            aux = aux->prox;
+            localidade->ruas[pontoA].principal = aux;  // Atualizar o ponteiro principal
+            free(temp);
+        }
+        // Remover outros nós
+        while (aux != NULL && aux->prox != NULL) {
+            if (aux->prox->destino == pontoB) {
+                No *temp = aux->prox;
+                aux->prox = temp->prox;
+                free(temp);
+            } else {
+                aux = aux->prox;
+            }
+        }
+        aux = localidade->ruas[pontoB].principal;
+        while (aux != NULL && aux->destino == pontoA) {
+            No *temp = aux;
+            aux = aux->prox;
+            localidade->ruas[pontoB].principal = aux;  // Atualizar o ponteiro principal
+            free(temp);
+        }
+
+        // Remover outros nós
+        while (aux != NULL && aux->prox != NULL) {
+            if (aux->prox->destino == pontoA) {
+                No *temp = aux->prox;
+                aux->prox = temp->prox;
+                free(temp);
+            } else {
+                aux = aux->prox;
+            }
+        }
 }
 /*
     Funcao para atualizar distancia(em metros) entre dois vértices do grafo (LOCALIDADE)
 */
 void actualizarDistancia(Localidade *localidade, int pontoA, int pontoB, int novaDistancia){
+    if(localidade == NULL){
+        printf("Localidade inexistente\n");
+        return;
+    }
+    if(!(localidade->numero_pontos>=pontoA && pontoA>0)){
+        printf("Ponto A inválido.\n");
+        return;
+    }
+    if(!(localidade->numero_pontos>=pontoB && pontoB>0)){
+        printf("Ponto B inválido.\n");
+        return;
+    }
     No * aux = NULL;
     if(localidade->ruas[pontoA].principal == NULL 
             || localidade->ruas[pontoB].principal == NULL){
@@ -171,6 +257,10 @@ void actualizarDistancia(Localidade *localidade, int pontoA, int pontoB, int nov
     Imprimir todas as ligações possíveis(Ai, Aj, distancia) no grafo (LOCALIDADE )
 */
 void imprimirLigacoes(Localidade *localidade){
+    if(localidade == NULL){
+        printf("Localidade inexistente.\n");
+        return;
+    }
     int i = 0;
     for(; i < localidade->numero_pontos; i++){
         No * aux = localidade->ruas[i + 1].principal;
@@ -183,7 +273,7 @@ void imprimirLigacoes(Localidade *localidade){
 
 void _imprimirMST(int parent[], int n) {
     for (int i = 1; i <= n; i++) {
-            printf ("%d <--> %d\n", parent[i], i);
+        printf ("%d <--> %d\n", parent[i], i);
     }
 }
 
@@ -233,11 +323,63 @@ void imprimirLigacoesOtimizadas(Localidade *localidade){
 }
 
 int menorDistanciaAB(Localidade * localidade, int pontoA, int pontoB){
+    if(localidade == NULL){
+        printf("Localidade inexistente.\n");
+        return -1;
+    }
+    if(!(localidade->numero_pontos>=pontoA && pontoA>0)){
+        printf("Ponto A inválido.\n");
+        return -1;
+    }
+    if(!(localidade->numero_pontos>=pontoB && pontoB>0)){
+        printf("Ponto B inválido.\n");
+        return -1;
+    }
     int menorDistancia = dijkstra(localidade, pontoA, pontoB);
     if(menorDistancia == INT_MAX){
         return -1;
     }
     return menorDistancia;
 }
-void adicionarPonto(Localidade * localidade, int pontoA, int pontoB, int distancia){}
-void destruirSimulacao(Localidade * localidade){}
+void adicionarPonto(Localidade * localidade, int pontoA){
+    if(localidade == NULL){
+        printf("Localidade inexistente!");
+        return;
+    }
+    if(pontoA > 0 && pontoA<= localidade->numero_pontos ){
+        if(localidade->ruas[pontoA].principal != NULL){
+            printf("Este ponto já existe!");
+            return;
+        }
+    }
+    int numPontosAnterior = localidade->numero_pontos;
+    localidade->numero_pontos = pontoA;
+    localidade->ruas = (AdjList *)realloc(localidade->ruas, (localidade->numero_pontos  + 1)* sizeof(AdjList));
+    
+    int i;
+    for(i = numPontosAnterior; i<=localidade->numero_pontos; i++)
+        localidade->ruas[i].principal = NULL;
+    
+    int numero_ligacoes;
+    printf("Quantas ligações deseja adicionar?\n");
+    scanf("%d", &numero_ligacoes);
+    int pontoB, distancia;
+    for(i = 0; i<numero_ligacoes; i++){
+        printf("%d - Digite o ponto e a distancia para a ligação do ponto %d\n", i + 1, pontoA);
+        scanf("%d%d", &pontoB, &distancia);
+        adicionarRua(localidade, pontoA, pontoB, distancia);
+    }
+    printf("Ponto adicionado com sucesso\n");
+}
+void destruirSimulacao(Localidade * localidade){
+    for (int i = 1; i <=localidade->numero_pontos; i++) {
+        No* noActual = localidade->ruas[i].principal;
+        while (noActual != NULL) {
+            No* noRemover = noActual;
+            noActual = noActual->prox;
+            free(noRemover);
+        }
+    }
+    free(localidade->ruas);
+    free(localidade);
+}
--- localidade.h
+++ localidade.h
@@ -19,12 +19,12 @@ typedef struct Localidade{
 void removerPonto(Localidade *localidade, int ponto);
 Localidade *criarLocalidade(int pontos, int ruas);
 void adicionarRua(Localidade *localidade, int pontoA, int pontoB, int distancia);
-Localidade *removerRua(Localidade *localidade, int pontoA, int pontoB);
+void removerRua(Localidade *localidade, int pontoA, int pontoB);
 void actualizarDistancia(Localidade *localidade, int pontoA, int pontoB, int novaDistancia);
 void imprimirLigacoes(Localidade *localidade);
 void imprimirLigacoesOtimizadas(Localidade *localidade);
 int menorDistanciaAB(Localidade * localidade, int pontoA, int pontoB);
-void adicionarPonto(Localidade * localidade, int pontoA, int pontoB, int distancia);
+void adicionarPonto(Localidade * localidade, int pontoA);
 void destruirSimulacao(Localidade * localidade);
 
 #endif
